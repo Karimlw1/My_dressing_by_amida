f6544412e091f6c2b7eed00dbdaaed2e22fb8830
lwano abdoul karim <karimabdoullw2@gmail.com>
2026-01-17 20:46:19 +0000

    implementing the project

---
+++ script.js
@@ -0,0 +1,35 @@
+
+let slides = document.querySelectorAll('.slide');
+
+let dots = document.querySelectorAll('.dot');
+
+let index = 0;
+
+
+
+function showSlide(i) {
+
+    slides.forEach(slide => slide.classList.remove('active'));
+
+    dots.forEach(dot => dot.classList.remove('active'));
+
+
+
+    slides[i].classList.add('active');
+
+    dots[i].classList.add('active');
+
+}
+
+
+
+setInterval(() => {
+
+    index++;
+
+    if (index >= slides.length) index = 0;
+
+    showSlide(index);
+
+}, 4000);
+
