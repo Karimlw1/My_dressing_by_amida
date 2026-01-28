44f47b88ade5484ff55aadcf10e348dcc45e1012
lwano abdoul karim <karimabdoullw2@gmail.com>
2026-01-29 02:19:03 +0300

    Update product.js

--- public/product.js
+++ public/product.js
@@ -485,9 +485,8 @@ if (product.options.color) {
 
 if (product.lieuDeLIvraison.ville) {
   lieuDeLIvraisonContainer.innerHTML += `
-    <label><i class="fa fa-list-ul"></i>
- Choisissez lieux de livraison 
-:</label>
+    <label><i class="fa fa-list-ul"> </i>
+  Choisissez le lieux de livraison : <br> [Kinshasa] , [Goma] , [Lubumbashi]</label>
     <select id="ville" required>
       ${product.lieuDeLIvraison.ville.map(v => `<option value="${v}">${v}</option>`).join("")}
     </select>
