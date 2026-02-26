9d679fa4687bc780d99bfbecc352b90b54d7a30a
Lwano Abdoul Karim <karimabdoullw2@gmail.com>
2026-02-26 21:31:13 +0300

    update

--- public/script.js
+++ public/script.js
@@ -98,7 +98,7 @@ fetch("/api/products")
         <img src="${product.image}" alt="${product.name}" />
         <div class="info">
           <div class="description">${product.name}</div>
-          <p class="category">${product.category}</p>
+          <p class="category2">${product.category}</p>
           <p class="size">${product.options?.size?.join(", ") || "N/A"}</p>
           <div class="price">$${product.price}</div>
           <button class="details-bttn" onclick="location.href='product.html?id=${product.id}'">
